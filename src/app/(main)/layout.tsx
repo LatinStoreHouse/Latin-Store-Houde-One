5c25f4a795760440ffae33cfd2ea2c500c436904
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-08-13 15:30:08 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -87,7 +87,7 @@ const getIconForSubItem = (label: string) => {
 }
 
 const Logo = () => (
-  <div className="relative h-10 w-32">
+  <div className="relative h-10 w-32" style={{ filter: 'invert(1) brightness(1.5)' }}>
     <Image src="/logo.png" alt="Latin Store House Logo" fill style={{ objectFit: 'contain' }} />
   </div>
 );
