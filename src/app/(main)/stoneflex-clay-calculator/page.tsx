8a37c874fc25a659a166e87472feb72243af06ea
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-08-20 18:55:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardDescription>

--- src/app/(main)/stoneflex-clay-calculator/page.tsx
+++ src/app/(main)/stoneflex-clay-calculator/page.tsx
@@ -1,8 +1,4 @@
 
-
-
-
-
 'use client';
 import React, { useState, useMemo, useEffect } from 'react';
 import Image from 'next/image';
@@ -29,7 +25,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 
 const WhatsAppIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current"><title>WhatsApp</title><path d="M12.04 2.018c-5.523 0-10 4.477-10 10s4.477 10 10 10c1.573 0 3.09-.37 4.49-1.035l3.493 1.032-1.06-3.39c.734-1.424 1.145-3.01 1.145-4.688.002-5.522-4.476-9.92-9.998-9.92zm3.328 12.353c-.15.27-.547.433-.945.513-.378.075-.826.104-1.312-.054-.933-.3-1.854-.9-2.61-1.68-.89-.897-1.472-1.95-1.63-2.93-.05-.293.003-.593.05-.86.06-.29.117-.582.26-.78.23-.32.512-.423.703-.408.19.012.36.003.504.003.144 0 .317.006.46.33.175.39.593 1.45.64 1.55.05.1.085.225.01.375-.074.15-.15.255-.255.36-.105.105-.204.224-.29.33-.085.105-.18.21-.074.405.23.45.983 1.416 1.95 2.13.772.58 1.48.74 1.83.656.35-.086.58-.33.725-.63.144-.3.11-.555.07-.643-.04-.09-.436-.51-.58-.68-.144-.17-.29-.26-.404-.16-.115.1-.26.15-.375.12-.114-.03-.26-.06-.375-.11-.116-.05-.17-.06-.24-.01-.07.05-.16.21-.21.28-.05.07-.1.08-.15.05-.05-.03-.21-.07-.36-.13-.15-.06-.8-.38-1.52-.98-.98-.82-1.65-1.85-1.72-2.02-.07-.17.08-1.3 1.3-1.3h.2c.114 0 .22.05.29.13.07.08.1.18.1.28l.02 1.35c0 .11-.05.22-.13.29-.08.07-.18-.1-.28-.1H9.98c-.11 0-.22-.05-.29-.13-.07-.08-.1-.18-.1-.28v-.15c0-.11.05-.22.13-.29-.08-.07-.18-.1-.28-.1h.02c.11 0 .22.05.29.13.07.08.1.18.1.28l.01.12c0 .11-.05.22-.13.29-.08.07-.18-.1-.28-.1h-.03c-.11 0-.22-.05-.29-.13-.07-.08-.1-.18-.1-.28v-.02c0-.11.05-.22.13-.29.08-.07-.18.1.28.1h.01c.11 0 .22-.05.29-.13.07.08.1.18.1.28a.38.38 0 0 0-.13-.29c-.08-.07-.18-.1-.28-.1z"/></svg>
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current"><title>WhatsApp</title><path d="M12.04 2.018c-5.523 0-10 4.477-10 10s4.477 10 10 10c1.573 0 3.09-.37 4.49-1.035l3.493 1.032-1.06-3.39c.734-1.424 1.145-3.01 1.145-4.688.002-5.522-4.476-9.92-9.998-9.92zm3.328 12.353c-.15.27-.547.433-.945.513-.378.075-.826.104-1.312-.054-.933-.3-1.854-.9-2.61-1.68-.89-.897-1.472-1.95-1.63-2.93-.05-.293.003-.593.05-.86.06-.29.117-.582.26-.78.23-.32.512-.423.703-.408.19.012.36.003.504.003.144 0 .317.006.46.33.175.39.593 1.45.64 1.55.05.1.085.225.01.375-.074.15-.15.255-.255.36-.105.105-.204.224-.29.33-.085.105-.18.21-.074.405.23.45.983 1.416 1.95 2.13.772.58 1.48.74 1.83.656.35-.086.58-.33.725-.63.144-.3.11-.555.07-.643-.04-.09-.436-.51-.58-.68-.144-.17-.29-.26-.404-.16-.115.1-.26.15-.375.12-.114-.03-.26-.06-.375-.11-.116-.05-.17-.06-.24-.01-.07.05-.16.21-.21.28-.05.07-.1.08-.15.05-.05-.03-.21-.07-.36-.13-.15-.06-.8-.38-1.52-.98-.98-.82-1.65-1.85-1.72-2.02-.07-.17.08-1.3 1.3-1.3h.2c.114 0 .22.05.29.13.07.08.1.18.1.28l.02 1.35c0 .11-.05.22-.13.29-.08.07-.18-.1-.28-.1H9.98c-.11 0-.22-.05-.29-.13-.07-.08-.1-.18-.1-.28v-.15c0-.11.05-.22.13-.29-.08-.07-.18.1-.28-.1h.02c.11 0 .22.05.29.13.07.08.1.18.1.28l.01.12c0 .11-.05.22-.13.29-.08.07-.18-.1-.28-.1h-.03c-.11 0-.22-.05-.29-.13-.07-.08-.1-.18-.1-.28v-.02c0-.11.05-.22.13-.29.08-.07-.18.1.28.1h.01c.11 0 .22-.05.29.13.07.08.1.18.1.28a.38.38 0 0 0-.13-.29c-.08-.07-.18-.1-.28-.1z"/></svg>
 );
 
 
@@ -203,38 +199,76 @@ function AdhesiveReferenceTable() {
     return (
         <DialogContent className="max-w-3xl">
             <DialogHeader>
-                <DialogTitle>Tabla de Referencia de Rendimiento de Adhesivo</DialogTitle>
-                <CardDescription>Unidades de adhesivo recomendadas por cada lámina.</CardDescription>
+                <DialogTitle>Tabla de Referencia de Insumos</DialogTitle>
+                <CardDescription>Unidades de adhesivo y rendimiento de sellante recomendadas.</CardDescription>
             </DialogHeader>
-            <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
-                <TabsList>
-                    {adhesiveReferenceByLine.map((lineData) => (
-                        <TabsTrigger value={lineData.line} key={lineData.line}>{lineData.line}</TabsTrigger>
-                    ))}
-                </TabsList>
-                {adhesiveReferenceByLine.map((lineData) => (
-                    <TabsContent value={lineData.line} key={lineData.line}>
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Referencias</TableHead>
-                                    <TableHead>Medida y Rendimiento Estándar</TableHead>
-                                    <TableHead>Medida y Rendimiento XL</TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {lineData.items.map(item => (
-                                    <TableRow key={item.reference}>
-                                        <TableCell className="font-medium">{item.reference}</TableCell>
-                                        <TableCell>{item.standard}</TableCell>
-                                        <TableCell>{item.xl}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TabsContent>
-                ))}
-            </Tabs>
+            
+            <div className="space-y-6">
+                <div>
+                    <h3 className="font-semibold mb-2">Rendimiento de Adhesivo por Lámina</h3>
+                    <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
+                        <TabsList>
+                            {adhesiveReferenceByLine.map((lineData) => (
+                                <TabsTrigger value={lineData.line} key={lineData.line}>{lineData.line}</TabsTrigger>
+                            ))}
+                        </TabsList>
+                        {adhesiveReferenceByLine.map((lineData) => (
+                            <TabsContent value={lineData.line} key={lineData.line}>
+                                <Table>
+                                    <TableHeader>
+                                        <TableRow>
+                                            <TableHead>Referencias</TableHead>
+                                            <TableHead>Medida y Rendimiento Estándar</TableHead>
+                                            <TableHead>Medida y Rendimiento XL</TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        {lineData.items.map(item => (
+                                            <TableRow key={item.reference}>
+                                                <TableCell className="font-medium">{item.reference}</TableCell>
+                                                <TableCell>{item.standard}</TableCell>
+                                                <TableCell>{item.xl}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TabsContent>
+                        ))}
+                    </Tabs>
+                </div>
+                
+                <Separator />
+
+                <div>
+                    <h3 className="font-semibold mb-2">Rendimiento de Sellantes</h3>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Tipo de Sellante</TableHead>
+                                <TableHead>Rendimiento por M²</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                             <TableRow>
+                                <TableCell>Semi-Brillante (Galón)</TableCell>
+                                <TableCell>60 M²</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Semi-Brillante (1/4 Galón)</TableCell>
+                                <TableCell>15 M²</TableCell>
+                            </TableRow>
+                             <TableRow>
+                                <TableCell>Brillante (Galón)</TableCell>
+                                <TableCell>40 M²</TableCell>
+                            </TableRow>
+                             <TableRow>
+                                <TableCell>Brillante (1/4 Galón)</TableCell>
+                                <TableCell>10 M²</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
         </DialogContent>
     );
 }
@@ -797,7 +831,7 @@ export default function StoneflexCalculatorPage() {
                     <DialogTrigger asChild>
                         <Button variant="outline" size="sm" className="ml-auto">
                             <HelpCircle className="mr-2 h-4 w-4" />
-                            Ver Tabla de Rendimiento de Adhesivo
+                            Ver Tabla de Rendimiento
                         </Button>
                     </DialogTrigger>
                     <AdhesiveReferenceTable />
@@ -1028,7 +1062,4 @@ export default function StoneflexCalculatorPage() {
   )
 }
 
-
-
-
-
+    
