db8be7f38382164e9c57e133b865224832ce40d4
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-08-28 18:20:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/lib/initial-containers.ts
+++ src/lib/initial-containers.ts
@@ -2,7 +2,7 @@
 export const initialContainers = [
     { id: 'MSCU1234567', eta: '2024-08-15', carrier: 'Maersk', status: 'En tránsito', products: [{ name: 'Cut stone', quantity: 200 }, { name: 'Travertino', quantity: 150 }], creationDate: '2024-07-01' },
     { id: 'CMAU7654321', eta: '2024-08-10', carrier: 'CMA CGM', status: 'Atrasado', products: [{ name: 'Black', quantity: 500 }], creationDate: '2024-07-05' },
-    { id: 'ARRIVED001', eta: '2024-07-20', carrier: 'MSC', status: 'Llegado', products: [{ name: 'Kund multy', quantity: 300 }], creationDate: '2024-07-02' },
+    { id: 'ARRIVED001', eta: '2024-07-20', carrier: 'MSC', status: 'En puerto', products: [{ name: 'Kund multy', quantity: 300 }], creationDate: '2024-07-02' },
     { id: 'PROD001', eta: '2024-09-30', carrier: 'MSC', status: 'En producción', products: [{ name: 'Concreto gris', quantity: 100 }], creationDate: '2024-07-25' },
     { id: 'PUERTO001', eta: '2024-08-01', carrier: 'Hapag-Lloyd', status: 'En puerto', products: [{ name: 'Carrara', quantity: 250 }], creationDate: '2024-07-10' },
 ];
