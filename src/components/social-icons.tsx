5a8d224458b045ebec1ab9f857a1bba9d7f5e85b
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-09-02 21:17:13 +0000

    .png es este ue quiero que uses, elimine todos los svg ahora todos son p

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -1,6 +1,7 @@
+
 'use client';
 import Image from 'next/image';
 
 export const WhatsAppIcon = ({ width = 32, height = 32 }: { width?: number; height?: number }) => (
-    <Image src="/imagenes/logos/Logo Whatsapp.svg" alt="WhatsApp" width={width} height={height} />
+    <Image src="/imagenes/logos/Logo Whatsapp.png" alt="WhatsApp" width={width} height={height} />
 );
