8377d39beed97e48c60b7836204cb0f0e8737f2c
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-09-09 16:13:58 +0000

    cambios

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -1,6 +1,2 @@
 import React from 'react';
 import Image from 'next/image';
-
-export const WhatsAppIcon = () => (
-    <Image src="/imagenes/logos/Logo Whatsapp.svg" alt="WhatsApp" width={32} height={32} />
-);
