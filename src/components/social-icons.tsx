ab94f9de0382dd17716aba2dcb7c577ad48a2b44
Latin Store House Colombia <proyectos.lsh@gmail.com>
2025-09-09 15:04:17 +0000

    Tengo un proyecto Next.js 15 que estoy desplegando en Firebase Hosting y

---
+++ src/components/social-icons.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Image from 'next/image';
+
+export const WhatsAppIcon = () => (
+    <Image src="/imagenes/logos/Logo Whatsapp.svg" alt="WhatsApp" width={32} height={32} />
+);
